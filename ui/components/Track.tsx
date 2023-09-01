e9db0cfdd10cfe901908f61508e2c9acdba8c02e
devark28 <82113556+devark28@users.noreply.github.com>
2023-09-01 23:59:38 +0300

    Update Track.tsx

--- ui/components/Track.tsx
+++ ui/components/Track.tsx
@@ -1,10 +1,14 @@
+import PlayArrowRoundedIcon from '@mui/icons-material/PlayArrowRounded'
+
 export default function Track() {
     return (
         <div className="flex w-full h-8 items-center local-track">
           <div className="flex items-center flex-[3] h-full">track name</div>
           <div className="flex items-center flex-[1] h-full">--:--</div>
           <div className="flex items-center flex-[4] h-full">artist</div>
-          <span className="flex items-center h-full">play</span>
+          <span className="flex items-center h-full">
+            <PlayArrowRoundedIcon/>
+          </span>
         </div>
     )
   }
